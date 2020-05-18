066d03204256044004f9fdd9c4f01ee7d36a03bd
Simon Heath <icefox@dreamquest.io>
2020-05-17 22:11:30 -0400

    String interner.
    Complete with a unit test!

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,26 @@
+//! Garnet compiler guts.
+//#![deny(missing_docs)]
+
 pub mod ast;
+pub mod intern;
 pub mod ir;
+
+/// Compilation context.  Contains things like symbol tables.
+/// Probably keeps most stages of compilation around to do
+/// things like output error messages or whatever?
+pub struct Cx {
+    /// Interned symbols
+    syms: intern::Interner,
+}
+
+impl Cx {
+    pub fn new() -> Self {
+        Cx {
+            syms: intern::Interner::default(),
+        }
+    }
+
+    pub fn intern(&mut self, s: &str) -> intern::Sym {
+        self.syms.intern(s)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+use garnet;
 use garnet::ast;
 
 fn main() {
+    let mut cx = garnet::Cx::new();
+    let mainsym = cx.intern("main");
+    let i32sym = cx.intern("i32");
     let ast = ast::Decl::Function {
-        name: ast::Symbol("main".into()),
+        name: ast::Symbol(mainsym),
         signature: ast::Signature {
             params: vec![],
-            rettype: ast::Type::Name(ast::Symbol("i32".into())),
+            rettype: ast::Type::Name(ast::Symbol(i32sym)),
         },
         body: vec![ast::Expr::Lit {
             val: ast::Literal::Integer(42),
