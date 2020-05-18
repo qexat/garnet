832b53edfc4822374d9329459b4c5f6f1c543ec0
Simon Heath <icefox@dreamquest.io>
2020-05-18 00:21:40 -0400

    oops main's gotta use fancy new features!

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,19 @@ fn main() {
     let mut cx = garnet::Cx::new();
     let mainsym = cx.intern("main");
     let i32sym = cx.intern("i32");
-    let ast = ast::Decl::Function {
-        name: ast::Symbol(mainsym),
-        signature: ast::Signature {
-            params: vec![],
-            rettype: ast::Type {
-                name: ast::Symbol(i32sym),
+    let ast = ast::Ast {
+        decls: vec![ast::Decl::Function {
+            name: ast::Symbol(mainsym),
+            signature: ast::Signature {
+                params: vec![],
+                rettype: ast::Type {
+                    name: ast::Symbol(i32sym),
+                },
             },
-        },
-        body: vec![ast::Expr::Lit {
-            val: ast::Literal::Integer(42),
+            body: vec![ast::Expr::int(42)],
         }],
     };
     println!("Hello, garnet: {:#?}", ast);
+    let ir = garnet::ir::lower(&ast);
+    println!("That becomes: {:#?}", ir);
 }
