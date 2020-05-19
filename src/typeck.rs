72f9370cd5ec7dca63717ed0db88c453c7f2bf40
Simon Heath <icefox@dreamquest.io>
2020-05-19 00:59:30 -0400

    Type check vars and literals

--- src/typeck.rs
+++ src/typeck.rs
@@ -7,8 +7,10 @@ use crate::intern::Sym;
 use crate::ir;
 use crate::Cx;
 
+#[derive(Debug, Clone)]
 pub enum TypeError {
     UnknownType(String),
+    UnknownVar(String),
 }
 
 /// A variable binding
@@ -50,6 +52,17 @@ impl Symtbl {
         let binding = VarBinding { name, typename };
         tbl.insert(name, binding);
     }
+
+    /// Get the type of the given variable, or an error
+    fn get_var(&self, cx: &Cx, name: Sym) -> Result<Sym, TypeError> {
+        for scope in self.syms.iter().rev() {
+            if let Some(binding) = scope.get(&name) {
+                return Ok(binding.typename);
+            }
+        }
+        let msg = format!("Unknown var: {}", cx.unintern(name));
+        Err(TypeError::UnknownVar(msg))
+    }
 }
 
 /// Does this type exist in the type table?
@@ -78,6 +91,7 @@ pub fn typecheck_decl(cx: &mut Cx, symtbl: &mut Symtbl, decl: &ir::Decl) -> Resu
             // Push scope, typecheck and add params to symbol table
             symtbl.push_scope();
             // TODO: Add the function itself!
+            // TODO: How to handle return statements, hm?
             for (pname, ptype) in signature.params.iter() {
                 if !type_exists(cx, ptype.name.0) {
                     let msg = format!(
@@ -107,7 +121,7 @@ fn typecheck_expr(cx: &mut Cx, symtbl: &mut Symtbl, expr: &ir::Expr) -> Result<S
     let ok = Ok(unit);
     match expr {
         Lit { val } => typecheck_literal(cx, val),
-        Var { name } => ok,
+        Var { name } => symtbl.get_var(cx, name.0),
         BinOp { op, lhs, rhs } => ok,
         UniOp { op, rhs } => ok,
         Block { body } => ok,
@@ -140,5 +154,33 @@ fn typecheck_literal(cx: &mut Cx, lit: &ir::Literal) -> Result<Sym, TypeError> {
         ir::Literal::Integer(_) => Ok(t_i32),
         ir::Literal::Bool(_) => Ok(t_bool),
         ir::Literal::Unit => Ok(t_unit),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Sanity check
+    #[test]
+    fn test_typecheck_lit() {
+        use ir;
+        let cx = &mut crate::Cx::new();
+        let t_i32 = cx.intern("i32");
+        let t_bool = cx.intern("bool");
+        let t_unit = cx.intern("()");
+        assert!(type_exists(cx, t_i32));
+        assert!(type_exists(cx, t_bool));
+        assert!(type_exists(cx, t_unit));
+
+        assert_eq!(
+            typecheck_literal(cx, &ir::Literal::Integer(9)).unwrap(),
+            t_i32
+        );
+        assert_eq!(
+            typecheck_literal(cx, &ir::Literal::Bool(false)).unwrap(),
+            t_bool
+        );
+        assert_eq!(typecheck_literal(cx, &ir::Literal::Unit).unwrap(), t_unit);
     }
 }
