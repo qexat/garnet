8a3eaaba283cc139e2eec791b1dd00d49fb720f4
Simon Heath <icefox@dreamquest.io>
2020-05-18 23:56:14 -0400

    Working on type checking.
    Need to refactor some to make symbols and stuff more uniform,
    but we're also going to need to include stuff like text spans
    for error reporting.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,22 @@
 //! Garnet compiler guts.
 //#![deny(missing_docs)]
 
+use std::collections::HashMap;
+
 pub mod ast;
 pub mod intern;
 pub mod ir;
+pub mod typeck;
+
+/// For now this is what we use as a type...
+/// This doesn't include the name, just the properties of it.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum TypeDef {
+    /// Signed integer with the given number of bytes
+    SInt(usize),
+    Bool,
+    Tuple(Vec<TypeDef>),
+}
 
 /// Compilation context.  Contains things like symbol tables.
 /// Probably keeps most stages of compilation around to do
@@ -11,16 +24,40 @@ pub mod ir;
 pub struct Cx {
     /// Interned symbols
     syms: intern::Interner,
+    /// Known types
+    types: HashMap<intern::Sym, TypeDef>,
 }
 
 impl Cx {
     pub fn new() -> Self {
-        Cx {
+        let mut s = Cx {
             syms: intern::Interner::default(),
-        }
+            types: HashMap::new(),
+        };
+        s.populate_builtin_types();
+        s
     }
 
+    /// Shortcut for getting an interned symbol.
     pub fn intern(&mut self, s: &str) -> intern::Sym {
         self.syms.intern(s)
+    }
+
+    pub fn unintern(&self, s: intern::Sym) -> &str {
+        self.syms.get(s)
+    }
+
+    /// Fill type table with whatever builtin types we have.
+    fn populate_builtin_types(&mut self) {
+        let types: HashMap<intern::Sym, TypeDef> = [
+            (self.intern("i32"), TypeDef::SInt(4)),
+            (self.intern("bool"), TypeDef::Bool),
+            // TODO: Not sure I like naming tuples like this.
+            (self.intern("()"), TypeDef::Tuple(vec![])),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+        self.types = types;
     }
 }
