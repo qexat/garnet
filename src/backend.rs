4e7156e88cacf8054455ded724ab040cfc511eaf
Simon Heath <icefox@dreamquest.io>
2020-05-26 21:04:43 -0400

    Huzzah, we can now output a real WASM program!

--- src/backend.rs
+++ src/backend.rs
@@ -15,27 +15,35 @@ use crate::*;
 /// preserve order of segments that are irrelevant to us.
 #[derive(Debug, Clone)]
 struct Module {
-    typesec: HashSet<elem::Type>,
-    codesec: HashSet<elem::FuncBody>,
+    /// Eventually we'll need to dedupe this but we need to preserve order too,
+    /// 'cause everything is resolved by indices, so for now we just fake this
+    typesec: Vec<elem::Type>,
+    /// For now assume we don't need to dedupe whole functions
+    codesec: Vec<elem::FuncBody>,
+    /// Function signatures
+    funcsec: Vec<elem::Func>,
+    /// Exports.  Name and typed reference.
+    exportsec: Vec<elem::ExportEntry>,
 }
 
 impl Module {
     fn new() -> Self {
         Module {
-            typesec: HashSet::new(),
-            codesec: HashSet::new(),
+            typesec: Vec::new(),
+            codesec: Vec::new(),
+            funcsec: Vec::new(),
+            exportsec: Vec::new(),
         }
     }
 
     /// Finally output the thing to actual wasm
     fn output(self) -> Result<Vec<u8>, elem::Error> {
-        let typesec = elem::Section::Type(elem::TypeSection::with_types(
-            self.typesec.into_iter().collect(),
-        ));
-        let codesec = elem::Section::Code(elem::CodeSection::with_bodies(
-            self.codesec.into_iter().collect(),
-        ));
-        let sections = vec![typesec, codesec];
+        let typesec = elem::Section::Type(elem::TypeSection::with_types(self.typesec));
+        let codesec = elem::Section::Code(elem::CodeSection::with_bodies(self.codesec));
+        let funcsec = elem::Section::Function(elem::FunctionSection::with_entries(self.funcsec));
+        let exportsec = elem::Section::Export(elem::ExportSection::with_entries(self.exportsec));
+        // TODO: The ordering of these sections matters!  Live and learn.
+        let sections = vec![typesec, funcsec, exportsec, codesec];
         let m = elem::Module::new(sections);
         m.to_bytes()
     }
@@ -59,7 +67,18 @@ fn compile_decl(cx: &mut Cx, m: &mut Module, decl: &ir::Decl) {
             body,
         } => {
             let sig = function_signature(cx, m, signature);
-            m.typesec.insert(elem::Type::Function(sig));
+            // TODO For now we don't try to dedupe types, just add 'em as redundantly
+            // as necessary.
+            let typesec_idx = m.typesec.len();
+            m.typesec.push(elem::Type::Function(sig));
+            let body = compile_function(cx, signature, body);
+            m.codesec.push(body);
+            let funcsec_idx = m.funcsec.len();
+            m.funcsec.push(elem::Func::new(typesec_idx as u32));
+            m.exportsec.push(elem::ExportEntry::new(
+                cx.unintern(*name).to_owned(),
+                elem::Internal::Function(funcsec_idx as u32),
+            ));
         }
         Decl::Const {
             name,
@@ -89,16 +108,37 @@ fn function_signature(cx: &mut Cx, m: &mut Module, sig: &ir::Signature) -> elem:
     elem::FunctionType::new(params, Some(rettype))
 }
 
-fn compile_expr(cx: &mut Cx, expr: &ir::Expr) {
-    use ir::*;
-    match expr {
-        Expr::Lit { val } => match val {
-            Literal::Integer(i) => {}
-            Literal::Bool(b) => (),
-            Literal::Unit => (),
-        },
-        _ => panic!("whlarg!"),
+/// Tracks local variables in a function for each type
+#[derive(Clone, Default, Debug)]
+struct Locals {
+    // TODO: Eventually fill these out with whatever var info we need
+    i32: Vec<()>,
+    i64: Vec<()>,
+    f32: Vec<()>,
+    f64: Vec<()>,
+}
+
+/// Compile a function, specifically -- wasm needs to know about its params and such.
+fn compile_function(cx: &mut Cx, sig: &ir::Signature, body: &[ir::Expr]) -> elem::FuncBody {
+    let mut locals = vec![];
+    let mut isns = vec![];
+
+    use elem::Instruction as I;
+    for expr in body {
+        use ir::*;
+        match expr {
+            Expr::Lit { val } => match val {
+                Literal::Integer(i) => isns.push(I::I32Const(*i as i32)),
+                Literal::Bool(b) => isns.push(I::I32Const(if *b { 1 } else { 0 })),
+                Literal::Unit => (),
+            },
+            _ => panic!("whlarg!"),
+        }
     }
+    // Functions must end with END
+    isns.push(I::End);
+
+    elem::FuncBody::new(locals, elem::Instructions::new(isns))
 }
 
 fn compile_type(cx: &Cx, t: &TypeDef) -> elem::ValueType {
