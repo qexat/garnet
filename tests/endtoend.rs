7a81f96ff84cdf57034650f5d0bc7d0f39885954
Simon Heath <icefox@dreamquest.io>
2020-05-28 10:24:22 -0400

    ...welp, we now have basic end-to-end tests with wasmtime.
    That was easy.

---
+++ tests/endtoend.rs
@@ -0,0 +1,46 @@
+/// End-to-end test rig: compile programs from source,
+/// or currently from AST,
+/// and ensure that they give the output we want.
+use garnet::{self, ast};
+use wasmtime as w;
+
+fn compile_wasm(bytes: &[u8]) -> w::Func {
+    // Set up settings and compile bytes
+    let store = w::Store::default();
+    let module = w::Module::new(&store, bytes).expect("Unvalid module");
+    // Create runtime env
+    let instance = w::Instance::new(&module, &[]).expect("Could not instantiate module");
+    // extract function
+    instance
+        .get_func("test")
+        .expect("Test function needs to be called 'test'")
+}
+
+#[test]
+fn addition() {
+    let mut cx = garnet::Cx::new();
+    let mainsym = cx.intern("test");
+    let i32_t = cx.intern_type(&garnet::TypeDef::SInt(4));
+    let ast = ast::Ast {
+        decls: vec![ast::Decl::Function {
+            name: mainsym,
+            signature: ast::Signature {
+                params: vec![],
+                rettype: i32_t,
+            },
+            //body: vec![ast::Expr::int(42)],
+            body: vec![ast::Expr::BinOp {
+                op: ast::BOp::Sub,
+                lhs: Box::new(ast::Expr::int(9)),
+                rhs: Box::new(ast::Expr::int(-3)),
+            }],
+        }],
+    };
+    let ir = garnet::ir::lower(&ast);
+    let wasm = garnet::backend::output(&mut cx, &ir);
+    // Compiling a function gets us a dynamically-typed thing.
+    // Assert what its type is and run it.
+    let f = compile_wasm(&wasm).get1::<(), i32>().unwrap();
+    let res: i32 = f(()).unwrap();
+    assert_eq!(res, 12);
+}
