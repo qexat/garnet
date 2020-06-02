16bc628510e92172569cd732826e14d9c7f96ea8
Simon Heath <icefox@dreamquest.io>
2020-06-01 23:23:31 -0400

    People are awful.
    This code on the other hand is pretty okayish, though far from ideal.

--- tests/endtoend.rs
+++ tests/endtoend.rs
@@ -4,6 +4,28 @@
 use garnet::{self, ast};
 use wasmtime as w;
 
+/* TODO: Hmmm, this is awkward 'cause of all the interning.
+ * How do we do this nicely without parsing?
+fn compile_garnet(ast: &ir::Ast) -> Vec<u8> {
+    let mut cx = garnet::Cx::new();
+    let mainsym = cx.intern("test");
+    let varsym = cx.intern("a");
+    let i32_t = cx.intern_type(&garnet::TypeDef::SInt(4));
+    let ast = ast::Ast {
+        decls: vec![ast::Decl::Function {
+            name: mainsym,
+            signature: ast::Signature {
+                params: vec![(varsym, i32_t)],
+                rettype: i32_t,
+            },
+            body: vec![ast::Expr::Var { name: varsym }],
+        }],
+    };
+    let ir = garnet::ir::lower(&ast);
+    garnet::backend::output(&mut cx, &ir)
+}
+*/
+
 fn compile_wasm(bytes: &[u8]) -> w::Func {
     // Set up settings and compile bytes
     let store = w::Store::default();
@@ -14,6 +36,31 @@ fn compile_wasm(bytes: &[u8]) -> w::Func {
     instance
         .get_func("test")
         .expect("Test function needs to be called 'test'")
+}
+
+#[test]
+fn var_lookup() {
+    let mut cx = garnet::Cx::new();
+    let mainsym = cx.intern("test");
+    let varsym = cx.intern("a");
+    let i32_t = cx.intern_type(&garnet::TypeDef::SInt(4));
+    let ast = ast::Ast {
+        decls: vec![ast::Decl::Function {
+            name: mainsym,
+            signature: ast::Signature {
+                params: vec![(varsym, i32_t)],
+                rettype: i32_t,
+            },
+            body: vec![ast::Expr::Var { name: varsym }],
+        }],
+    };
+    let ir = garnet::ir::lower(&ast);
+    let wasm = garnet::backend::output(&mut cx, &ir);
+    // Compiling a function gets us a dynamically-typed thing.
+    // Assert what its type is and run it.
+    let f = compile_wasm(&wasm).get1::<i32, i32>().unwrap();
+    let res: i32 = f(5).unwrap();
+    assert_eq!(res, 5);
 }
 
 #[test]
