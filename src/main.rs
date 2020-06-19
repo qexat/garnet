b99ffb4e2d7b4433816ba9b6b67b9eb2b94c036c
Simon Heath <icefox@dreamquest.io>
2020-06-19 16:49:17 -0400

    Trying out picoargs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,36 @@
 use std::path::PathBuf;
 
-use structopt::StructOpt;
+use pico_args;
 
 use garnet;
 
-#[derive(StructOpt, Debug)]
 struct Opt {
-    #[structopt(name = "FILE", parse(from_os_str))]
-    file: PathBuf,
+    files: Vec<PathBuf>,
+}
+
+fn parse_args() -> Opt {
+    let args = pico_args::Arguments::from_env();
+    let files = args.free().unwrap().iter().map(PathBuf::from).collect();
+    Opt { files: files }
+}
+
+fn help() {
+    println!("TODO: help text generator.");
 }
 
 fn main() -> std::io::Result<()> {
-    let opt = Opt::from_args();
-    let src = std::fs::read_to_string(&opt.file)?;
-    let output = garnet::compile(&src);
+    let opt = parse_args();
     // Output to file
-    let mut output_file = opt.file.clone();
-    output_file.set_extension("wasm");
-    std::fs::write(&output_file, &output)
+    if opt.files.len() == 0 {
+        help();
+        return Ok(());
+    }
+    for file in opt.files {
+        let src = std::fs::read_to_string(&file)?;
+        let output = garnet::compile(&src);
+        let mut output_file = file.clone();
+        output_file.set_extension("wasm");
+        std::fs::write(&output_file, &output)?;
+    }
+    return Ok(());
 }
