5f9ba0ae3ef2c1bef7bc4f328f0d21fa7890e164
Simon Heath <icefox@dreamquest.io>
2020-07-05 17:17:58 -0400

    Ok well so far SSA doesn't seem a *bad* idea...?

--- src/lir.rs
+++ src/lir.rs
@@ -17,7 +17,8 @@
 
 use std::collections::HashMap;
 
-use crate::{TypeSym, VarSym};
+use crate::ir;
+use crate::{Cx, TypeSym, VarSym};
 
 /// The type of a var/virtual register, just a number
 #[derive(Debug, Copy, Clone, PartialEq)]
@@ -30,26 +31,40 @@ pub struct BB(usize);
 /// A function
 pub struct Func {
     name: VarSym,
-    params: Vec<TypeSym>,
+    params: Vec<(Var, TypeSym)>,
     returns: TypeSym,
-    body: HashMap<BB, Vec<Instr>>,
+    body: HashMap<BB, Block>,
+}
+
+/// The various ways we can end a basic block
+/// Apparently a switch instruction also makes using
+/// this far easier, and compiling things somewhat
+/// harder.  Something to think about.
+pub enum Branch {
+    /// Unconditional jump
+    Jump(Var, BB),
+    /// Conditional branch
+    Branch(Var, BB, BB),
+    Return(Option<Var>),
+    Unreachable,
+}
+
+/// The actual type of a basic block
+pub struct Block {
+    id: BB,
+    body: Vec<Instr>,
+    terminator: Branch,
 }
 
 /// an IR instruction
 pub enum Instr {
     Assign(Var, Op),
-    /// Conditional branch
-    Branch(Var, BB, BB),
-    /// Unconditional jump
-    Jump(Var, BB),
-    Return(Var),
 }
 
 /// Operations/rvalues
 pub enum Op {
     // Values
     ValI32(i32),
-    ValBool(bool),
 
     // Operators
     AddI32(Var, Var),
@@ -57,7 +72,7 @@ pub enum Op {
     MulI32(Var, Var),
     DivI32(Var, Var),
     NegI32(Var),
-    NotBool(Var),
+    NotI32(Var),
 
     // Memory
     AddrOf(Var),
@@ -65,6 +80,99 @@ pub enum Op {
     StoreI32(Var),
 
     // Control flow
+    // TODO: phi can take any number of args
     Phi(BB, BB),
     Call(Var, VarSym, Vec<Var>),
+}
+
+pub struct FuncBuilder {
+    func: Func,
+    next_var: usize,
+    next_bb: usize,
+}
+
+impl FuncBuilder {
+    pub fn new(func_name: VarSym, params: &[TypeSym], rettype: TypeSym) -> Self {
+        let mut res = Self {
+            func: Func {
+                name: func_name,
+                params: vec![],
+                returns: rettype,
+                body: HashMap::new(),
+            },
+            next_var: 0,
+            next_bb: 0,
+        };
+        let new_params = params.iter().map(|t| (res.next_var(), *t)).collect();
+        res.func.params = new_params;
+        res
+    }
+
+    fn next_var(&mut self) -> Var {
+        let s = Var(self.next_var);
+        self.next_var += 1;
+        s
+    }
+
+    fn next_block(&mut self) -> Block {
+        let id = BB(self.next_bb);
+        self.next_bb += 1;
+        let block = Block {
+            id,
+            body: vec![],
+            terminator: Branch::Unreachable,
+        };
+        block
+    }
+
+    fn build(self) -> Func {
+        self.func
+    }
+
+    /// Build a new instruction
+    /// apparently currently the only option is Assign?
+    ///
+    fn assign(&mut self, bb: &mut Block, op: Op) -> Var {
+        let v = self.next_var();
+        let instr = Instr::Assign(v, op);
+        bb.body.push(instr);
+        v
+    }
+}
+
+pub fn lower_decl(cx: &Cx, decl: &ir::Decl<TypeSym>) {
+    match decl {
+        ir::Decl::Function {
+            name,
+            signature,
+            body,
+        } => {
+            // TODO: Map param names somehow...
+            let params: Vec<_> = signature.params.iter().map(|(name, ty)| *ty).collect();
+            let mut fb = FuncBuilder::new(*name, params.as_ref(), signature.rettype);
+            todo!()
+        }
+        _ => todo!(),
+    }
+}
+
+fn lower_expr(cx: &Cx, fb: &mut FuncBuilder, bb: &mut Block, expr: &ir::TypedExpr<TypeSym>) {
+    use ir::Expr as E;
+    use ir::*;
+    match &expr.e {
+        E::Lit { val } => match val {
+            Literal::Integer(i) => {
+                assert!(*i < (std::i32::MAX as i64));
+                let op = Op::ValI32(*i as i32);
+                fb.assign(bb, op);
+            }
+            // Turn bools into integers.
+            Literal::Bool(b) => {
+                let op = Op::ValI32(if *b { 1 } else { 0 });
+                fb.assign(bb, op);
+            }
+        },
+
+        _ => todo!(),
+    }
 }
