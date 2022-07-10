f9af30e479c75b1adbd9f57d6c6f1354ee75b966
Simon Heath <icefox@dreamquest.io>
2022-07-10 17:02:44 -0400

    FINE everyone keeps saying HM is so easy, let's just fucking do it!

---
+++ fml/src/lib.rs
@@ -0,0 +1,106 @@
+//! Garnet compiler guts.
+//#![deny(missing_docs)]
+/*
+ * type t =
+    Type of int
+  | Type_Constructor of int * t list
+  | Type_Variable of t option ref
+
+exception Not_Equal of t * t
+exception Infinite_Type of t option ref * t
+
+let rec occurs v_a = function
+  | Type _ -> false
+  | Type_Constructor (_, ts_b) -> List.exists (occurs v_a) ts_b
+  | Type_Variable v_b ->
+    match !v_b with
+    | None -> v_a == v_b
+    | Some t -> occurs v_a t
+
+let rec deref t =
+  match t with
+  | Type_Variable v_t ->
+    begin match !v_t with
+    | None -> t
+    | Some t2 -> deref t2
+    end
+  | _ -> t
+
+let rec unify a b =
+  let a = deref a in
+  let b = deref b in
+  match a, b with
+    | Type t_a, Type t_b ->
+      if t_a != t_b then
+        raise (Not_Equal (a, b))
+    | Type_Constructor (c_a, ts_a), Type_Constructor (c_b, ts_b) ->
+      if c_a != c_b then
+        raise (Not_Equal (a, b))
+      else
+        List.iter2 unify ts_a ts_b
+    | Type _, Type_Constructor _ -> raise (Not_Equal (a, b))
+    | Type_Constructor _, Type _ -> raise (Not_Equal (a, b))
+    | Type_Variable v_a, Type_Variable v_b ->
+      if not (v_a == v_b) then
+        v_a := Some b;
+    | Type_Variable v_a, _ ->
+      if occurs v_a b then
+        raise (Infinite_Type (v_a, b))
+      else
+        v_a := Some b
+    | _, Type_Variable v_b -> unify b a
+
+
+let fn_tycon = 0
+let list_tycon = 1
+let int_type = 2
+
+(* forall a b. (a -> b) -> List a -> List b *)
+let t_map =
+  let a = Type_Variable (ref None) in
+  let b = Type_Variable (ref None) in
+  Type_Constructor (0,
+   [Type_Constructor (0, [a; b]);
+    Type_Constructor (0,
+     [Type_Constructor (1, [a]);
+      Type_Constructor (1, [b])])])
+
+let t_double = Type_Constructor (0, [Type 2; Type 2])
+
+let apply f x =
+  let a = Type_Variable (ref None) in
+  let b = Type_Variable (ref None) in
+  (* Check that f is a function. *)
+  unify f (Type_Constructor (0, [a; b]));
+  (* Unify x with the argument of f. *)
+  unify a x;
+  (* Return the result type *)
+  x
+
+(* List Int -> List Int *)
+(* Type_Constructor (0, [Type 2; Type 2]) *)
+let example = apply t_map t_double
+ */
+
+pub mod ast;
+pub mod parser;
+
+/// A complete-ish description of a type.
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum TypeDef {
+    Type(i32),
+    TypeConstr(i32, Vec<TypeDef>),
+    TypeVar(Option<Box<TypeDef>>),
+}
+
+pub fn typecheck(ast: &ast::Ast) {}
+
+pub fn compile(filename: &str, src: &str) -> Vec<u8> {
+    let ast = {
+        let mut parser = parser::Parser::new(filename, src);
+        parser.parse()
+    };
+    typecheck(&ast);
+    let res = format!("AST:\n{:#?}", ast);
+    res.as_bytes().to_owned()
+}
