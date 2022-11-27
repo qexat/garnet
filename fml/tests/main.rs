7caef3cdb436458a4ba6f870d06b0c72a68f042b
Simon Heath <icefox@dreamquest.io>
2022-11-27 17:13:55 -0500

    Ok I didn't expect that to work but apparently it does

--- fml/tests/main.rs
+++ fml/tests/main.rs
@@ -7,6 +7,12 @@ fn test_thing() {
 }
 
 #[test]
+fn test_thing2() {
+    let src = include_str!("test2.gt");
+    let _output = fml::compile("test2.gt", src);
+}
+
+#[test]
 #[should_panic]
 fn test_failure() {
     let src = include_str!("test_failure.gt");
