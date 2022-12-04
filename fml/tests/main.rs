9680f77d92190daefb6cac654dfae16ffd0dee6f
Simon Heath <icefox@dreamquest.io>
2022-12-04 15:31:07 -0500

    aaaaaaaaaa lambdas appear to work

--- fml/tests/main.rs
+++ fml/tests/main.rs
@@ -44,3 +44,34 @@ fn test_tuple3() {
     let src = include_str!("test_tuple3.gt");
     let _output = fml::compile("test_tuple3.gt", src);
 }
+
+#[test]
+fn test_lambda1() {
+    let src = include_str!("test_lambda1.gt");
+    let _output = fml::compile("test_lambda1.gt", src);
+}
+
+#[test]
+fn test_lambda2() {
+    let src = include_str!("test_lambda2.gt");
+    let _output = fml::compile("test_lambda2.gt", src);
+}
+
+#[test]
+fn test_lambda3() {
+    let src = include_str!("test_lambda3.gt");
+    let _output = fml::compile("test_lambda3.gt", src);
+}
+
+#[test]
+fn test_lambda4() {
+    let src = include_str!("test_lambda4.gt");
+    let _output = fml::compile("test_lambda4.gt", src);
+}
+
+#[test]
+#[should_panic]
+fn test_lambda5() {
+    let src = include_str!("test_lambda5.gt");
+    let _output = fml::compile("test_lambda5.gt", src);
+}
