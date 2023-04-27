76d633b3a4ba69b9c4b2bacde3736f9a93791e39
Simon Heath <icefox@dreamquest.io>
2023-04-27 17:44:11 -0400

    need more caffeine apparently.

--- src/passes/type_erasure.rs
+++ src/passes/type_erasure.rs
@@ -10,12 +10,50 @@
 
 use crate::passes::*;
 
-fn type_erase_expr(expr: ExprNode, tck: &mut typeck::Tck) -> ExprNode {
-    expr
+fn type_erase_type(typ: Type) -> Type {
+    match typ {
+        Type::Prim(_) => typ,
+        Type::Enum(_) => typ,
+        Type::Named(_, _) => todo!(),
+        Type::Func(_, _, _) => todo!(),
+        Type::Struct(_, _) => todo!(),
+        Type::Sum(_, _) => todo!(),
+        Type::Array(_, _) => todo!(),
+        Type::Generic(_) => todo!(),
+    }
 }
 
-fn type_erase_type(typ: Type) -> Type {
-    typ
+fn type_erase_expr(expr: ExprNode, tck: &mut typeck::Tck) -> ExprNode {
+    let expr_typeid = tck.get_expr_type(&expr);
+    let expr_type = tck.reconstruct(expr_typeid).expect("Should never happen");
+    let new_contents = match (*expr.e).clone() {
+        E::Let {
+            varname,
+            typename,
+            init,
+            mutable,
+        } => {
+            todo!()
+        }
+        E::StructCtor { body } => todo!(),
+        E::StructRef {
+            expr: inner_expr,
+            elt,
+        } => {
+            todo!()
+        }
+        other => other,
+    };
+    // Change the type of the struct literal expr node to the new tuple
+    // We need to do this to any expression because its type may be a struct.
+    // And we need to do this after the above because we've changed the
+    let new_t = type_erase_type(expr_type.clone());
+    if new_t != expr_type {
+        info!("Replaced generic type {:?} with {:?}", expr_type, new_t);
+        trace!("Expression was rewritten into {:?}", new_contents);
+    }
+    tck.replace_expr_type(&expr, &new_t);
+    expr.map(&mut |_| new_contents.clone())
 }
 
 pub(super) fn type_erasure(ir: Ir, tck: &mut typeck::Tck) -> Ir {
