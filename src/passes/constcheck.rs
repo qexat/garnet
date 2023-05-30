9f95b100da17ce5158868d9d7833854d0d103ed2
Simon Heath <icefox@dreamquest.io>
2023-05-30 15:10:32 -0400

    Ok I think we can make test_eq_module pass by adding const annotations.

---
+++ src/passes/constcheck.rs
@@ -0,0 +1,9 @@
+//! Ok this is the skeleton of a Pass that will mark expressions/decls
+//! as const, basically inferring them down the chain.
+//! Shoooooould be pretty easy: some terminal expressions such as
+//! assignment, loops, and such are non const, and if something contains
+//! any non-const expressions it is not const.  We start off by assuming
+//! nothing is const and annotating it downward from leaves to roots.
+//!
+//! We might need to do things like make sure we mark main() and
+//! println functions as non-const somehow, we will see.
