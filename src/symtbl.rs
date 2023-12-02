88cdab100112aec3e026675abcd719181e37720a
Simon Heath <icefox@dreamquest.io>
2023-12-02 15:54:53 -0500

    Ok this is slooooowly taking shape.

--- src/symtbl.rs
+++ src/symtbl.rs
@@ -11,22 +11,43 @@ use std::collections::BTreeMap;
 use std::rc::Rc;
 
 use crate::*;
-// TODO: Dependency inversion
-use crate::typeck::TypeId;
 
-#[derive(Clone, Default)]
+/// A symbol that has been renamed to be globally unique.
+/// So basically instead of `foo` being scoped, it gets
+/// renamed to `foo_1` where every mention of `foo_1`
+/// refers to the same value.
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+struct UniqueSym(Sym);
+
+#[derive(Clone, Default, Debug)]
 struct ScopeFrame {
-    /// Values are (type, mutability)
-    symbols: BTreeMap<Sym, (TypeId, bool)>,
-    types: BTreeMap<Sym, Type>,
+    symbols: BTreeMap<Sym, UniqueSym>,
+    _types: BTreeMap<Sym, Type>,
 }
+
+#[derive(Clone, Debug, Default)]
+struct SymbolInfo {}
 
 /// Basic symbol table that maps names to type ID's
 /// and manages scope.
 /// Looks ugly, works well.
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Symtbl {
     frames: Rc<RefCell<Vec<ScopeFrame>>>,
+    /// A mapping from unique symbol names to whatever
+    /// we need to know about them.
+    unique_symbols: BTreeMap<UniqueSym, SymbolInfo>,
+
+    /// Every time we generate a new symbol we increment
+    /// this.
+    ///
+    /// TODO: It'd be kinda nice to have foo_1 , bar_1, foo_2,
+    /// baz_1, foo_3 etc but don't need it for now.
+    ///
+    /// Also it might be nice to have each variable have a unique
+    /// number someday.  ...though symbols already kinda do, hm.
+    /// That can go in the SymbolInfo tho.
+    gensym_increment: usize,
 }
 
 impl Default for Symtbl {
@@ -35,6 +56,8 @@ impl Default for Symtbl {
     fn default() -> Self {
         Self {
             frames: Rc::new(RefCell::new(vec![ScopeFrame::default()])),
+            unique_symbols: Default::default(),
+            gensym_increment: 0,
         }
     }
 }
@@ -54,55 +77,206 @@ impl Drop for ScopeGuard {
 }
 
 impl Symtbl {
-    pub(crate) fn push_scope(&self) -> ScopeGuard {
+    /// Creates a new symbol with the name of the given one
+    /// and a unique numerical suffix.
+    fn gensym(&mut self, base: Sym) -> UniqueSym {
+        self.gensym_increment += 1;
+        let name = format!("{}_{}", &*base.val(), self.gensym_increment);
+        UniqueSym(Sym::new(name))
+    }
+
+    fn push_scope(&self) -> ScopeGuard {
         self.frames.borrow_mut().push(ScopeFrame::default());
         ScopeGuard {
             scope: self.clone(),
         }
     }
 
-    pub fn add_var(&self, var: Sym, ty: TypeId, mutable: bool) {
+    /// Returns the UniqueSym that the given Sym is *currently*
+    /// bound to, or None if DNE
+    fn get_binding(&self, sym: Sym) -> Option<UniqueSym> {
+        for scope in self.frames.borrow().iter().rev() {
+            let v = scope.symbols.get(&sym).cloned();
+            if v.is_some() {
+                return v;
+            }
+        }
+        None
+    }
+
+    /// Get a reference to the symbol most recently bound
+    /// with that name, or panic if it does not exist
+    fn really_get_binding(&self, sym: Sym) -> UniqueSym {
+        let msg = format!("Symbol {} is not bound!", sym);
+        self.get_binding(sym).expect(&msg)
+    }
+
+    /// Get the SymbolInfo for the given UniqueSym, or
+    /// None if DNE
+    fn get_info(&self, sym: UniqueSym) -> Option<&SymbolInfo> {
+        self.unique_symbols.get(&sym)
+    }
+
+    fn binding_exists(&self, sym: Sym) -> bool {
+        self.get_binding(sym).is_some()
+    }
+
+    fn unique_exists(&self, sym: UniqueSym) -> bool {
+        self.get_info(sym).is_some()
+    }
+
+    /// Takes a symbol, generates a new UniqueSym for it,
+    /// and stuffs it into the topmost scope, overwriting
+    /// any symbol of the same name already there.
+    fn bind_new_symbol(&mut self, sym: Sym) -> UniqueSym {
+        let newsym = self.gensym(sym);
         self.frames
             .borrow_mut()
             .last_mut()
             .expect("Scope stack underflow")
             .symbols
-            .insert(var, (ty, mutable));
+            .insert(sym, newsym);
+        self.unique_symbols.insert(newsym, SymbolInfo::default());
+        newsym
     }
 
-    /// Checks whether the var exists in the currently alive scopes
-    pub fn get_var_binding(&self, var: Sym) -> Option<(TypeId, bool)> {
-        for scope in self.frames.borrow().iter().rev() {
-            let v = scope.symbols.get(&var);
-            if v.is_some() {
-                return v.cloned();
-            }
+    /// Introduce a new symbol, or panic if that name is already
+    /// defined (ie, it is a name conflict, like two functions with
+    /// the same name)
+    fn new_unique_symbol(&mut self, sym: Sym) -> UniqueSym {
+        if self.binding_exists(sym) {
+            panic!("Attempting to create duplicate symbol {}", sym);
+        } else {
+            self.bind_new_symbol(sym)
         }
-        None
     }
 
-    pub(crate) fn add_type(&self, name: Sym, ty: &Type) {
-        self.frames
+    /// Introduce a new symbol as long as it doesn't clash with one
+    /// currently in the top scope.
+    fn new_local_symbol(&mut self, sym: Sym) -> UniqueSym {
+        // Does the symbol exist in the topmost scope?
+        if self
+            .frames
             .borrow_mut()
             .last_mut()
             .expect("Scope stack underflow")
-            .types
-            .insert(name, ty.to_owned());
+            .symbols
+            .contains_key(&sym)
+        {
+            panic!("Attempting to create duplicate global symbol {}", sym)
+        } else {
+            self.bind_new_symbol(sym)
+        }
     }
-
-    pub(crate) fn get_type(&self, ty: Sym) -> Option<Type> {
-        for scope in self.frames.borrow().iter().rev() {
-            let v = scope.types.get(&ty);
-            if v.is_some() {
-                return v.cloned();
+    fn handle_exprs(&mut self, exprs: &[hir::ExprNode]) {
+        for expr in exprs {
+            self.handle_expr(expr);
+        }
+    }
+    fn handle_expr(&mut self, expr: &hir::ExprNode) {
+        use hir::Expr::*;
+        match &*expr.e {
+            Lit { .. } => (),
+            Var { name } => {
+                let _ = self.really_get_binding(*name);
             }
+            BinOp { lhs, rhs, .. } => {
+                self.handle_expr(lhs);
+                self.handle_expr(rhs);
+            }
+            UniOp { rhs, .. } => {
+                self.handle_expr(rhs);
+            }
+            Block { body } => {
+                let _guard = self.push_scope();
+                self.handle_exprs(body);
+            }
+            Let { varname, init, .. } => {
+                // init expression cannot refer to the same
+                // symbol name
+                self.handle_expr(init);
+                self.bind_new_symbol(*varname);
+            }
+            If { cases } => {
+                for (test, body) in cases {
+                todo!()
+                    
+                }
+            }
+            _ => todo!(),
         }
-        None
     }
+}
 
-    pub fn for_toplevel(&self, f: impl Fn(Sym, TypeId)) {
-        for (name, (id, _)) in &self.frames.borrow().last().unwrap().symbols {
-            f(*name, *id)
+fn predeclare_decls(symtbl: &mut Symtbl, decls: &[hir::Decl]) {
+    use hir::Decl::*;
+    for d in decls {
+        match d {
+            Function {
+                name,
+                signature: _,
+                body: _,
+            } => {
+                symtbl.new_unique_symbol(*name);
+            }
+            TypeDef {
+                name: _,
+                params: _,
+                typedecl: _,
+            } => {
+                //todo!()
+            }
+            Const {
+                name,
+                typ: _,
+                init: _,
+            } => {
+                symtbl.new_unique_symbol(*name);
+            }
+            Import { .. } => todo!(),
         }
     }
+}
+
+fn handle_decls(symtbl: &mut Symtbl, decls: &[hir::Decl]) {
+    use hir::Decl::*;
+    for d in decls {
+        match d {
+            Function {
+                name: _,
+                signature,
+                body,
+            } => {
+                let _scope = symtbl.push_scope();
+                for (paramname, _) in &signature.params {
+                    symtbl.bind_new_symbol(*paramname);
+                }
+                symtbl.handle_exprs(body);
+            }
+            TypeDef {
+                name: _,
+                params: _,
+                typedecl: _,
+            } => {
+                //todo!()
+            }
+            Const {
+                name: _,
+                typ: _,
+                init: _,
+            } => {
+                todo!()
+            }
+            Import { .. } => todo!(),
+        }
+    }
+}
+
+/// Takes all value and type names and renames them all to
+/// remove scope dependence.
+pub fn resolve_symbols(ir: hir::Ir) -> (hir::Ir, Symtbl) {
+    let mut s = Symtbl::default();
+    predeclare_decls(&mut s, &ir.decls);
+    handle_decls(&mut s, &ir.decls);
+    (ir, s)
 }
