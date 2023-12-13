dd597f977f73a2a4ea17849c0ec25e9f331698ff
Simon Heath <icefox@dreamquest.io>
2023-12-13 08:20:12 -0500

    Alpha renaming appears to work now~

--- src/lib.rs
+++ src/lib.rs
@@ -614,9 +614,9 @@ pub fn try_compile(
     };
     let hir = hir::lower(&ast);
     info!("HIR from AST lowering:\n{}", &hir);
-    let (hir, symtbl) = symtbl::resolve_symbols(hir);
+    let (hir, _symtbl) = symtbl::resolve_symbols(hir);
     info!("HIR from symtbl renaming:\n{}", &hir);
-    info!("Symtbl from AST:\n{:#?}", &symtbl);
+    //info!("Symtbl from AST:\n{:#?}", &symtbl);
     let hir = passes::run_passes(hir);
     let tck = &mut typeck::typecheck(&hir)?;
     borrowck::borrowck(&hir, tck).unwrap();
